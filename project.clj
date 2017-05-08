02d72526b16a46921574012b89bda62a7ff9419b
Richard Wofford <rkwofford@gmail.com>
2017-05-07 20:57:19 -0400

    Update project.clj

--- project.clj
+++ project.clj
@@ -5,7 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [incanter "1.9.0"]
-                 [seesaw "1.4.5"]
                  ]
   :main ^:skip-aot toolbox.core
   :target-path "target/%s"
